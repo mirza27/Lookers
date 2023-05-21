308be3e224d41f5d25fe920b01b696603dec664e
andraalayubi <59492315+andraalayubi@users.noreply.github.com>
2023-05-21 15:24:56 +0700

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -2,3 +2,95 @@ const express = require('express');
 const router = express.Router();
 const db =  require('db');
 
+//andra
+const express = require('express');
+const router = express.Router();
+const { Pool } = require('pg');
+
+const app = express();
+const port = 3000;
+
+// Konfigurasi koneksi PostgreSQL
+const pool = new Pool({
+    user: 'postgres',
+    host: 'containers-us-west-187.railway.app',
+    database: 'railway',
+    password: 'q1tXYJw2zY5aXZVh9NQW',
+    port: 7572,
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Halaman login
+app.get('/login', (req, res) => {
+    res.send(`
+    <form method="POST" action="/login">
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <button type="submit">Login</button>
+    </form>
+    `);
+});
+
+// Proses login
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    // Periksa kecocokan data login dengan data di database
+    pool.query('SELECT * FROM users WHERE username = $1 AND password = $2', [username, password], (err, result) => {
+        if (err) {
+            console.log(err);
+            res.send('Terjadi kesalahan saat melakukan login.');
+        } else {
+            if (result.rowCount === 1) {
+                res.send('Login berhasil!');
+            } else {
+                res.send('Username atau password salah.');
+            }
+        }
+    });
+});
+
+// Halaman register
+app.get('/register', (req, res) => {
+    res.send(`
+        <form method="POST" action="/register">
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <button type="submit">Register</button>
+        </form>
+    `);
+});
+
+// Proses register
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+
+    // Periksa apakah username sudah ada di database
+    pool.query('SELECT * FROM users WHERE username = $1', [username], (err, result) => {
+        if (err) {
+            console.log(err);
+            res.send('Terjadi kesalahan saat melakukan register.');
+        } else {
+            if (result.rowCount === 0) {
+                // Tambahkan user baru ke database
+                pool.query('INSERT INTO users VALUES ($1, $2)', [username, password], (err, result) => {
+                    if (err) {
+                        console.log(err);
+                        res.send('Terjadi kesalahan saat melakukan register.');
+                    } else {
+                        res.send('Registrasi berhasil!');
+                    }
+                });
+            } else {
+                res.send('Username sudah terdaftar.');
+            }
+        }
+    });
+});
+
+// Jalankan server
+app.listen(port, () => {
+    console.log(`Server berjalan di http://localhost:${port}`);
+});
