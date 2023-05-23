824895326822f7badcbc98e2ae897237de9f3d13
andraalayubi <59492315+andraalayubi@users.noreply.github.com>
2023-05-23 07:03:54 +0700

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const app = express();
-const db =  require('../db/db');
-
-app.set('view engine', 'ejs');
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+const db = require('../db/db');
+const pgp = db.$config.pgp;
 
 // Halaman login
 router.get('/', (req, res) => {
-    res.render("login.ejs");
+    res.render('login.ejs');
 });
 
 // Proses login
@@ -18,17 +13,14 @@ router.post('/', (req, res) => {
     const { username, password } = req.body;
 
     // Periksa kecocokan data login dengan data di database
-    db.query('SELECT * FROM users WHERE username = $1 AND password = $2', [username, password], (err, result) => {
-        if (err) {
-            console.log(err);
-            res.send('Terjadi kesalahan saat melakukan login.');
-        } else {
-            if (result.rowCount === 1) {
-                res.send('Login berhasil!');
-            } else {
-                res.send('Username atau password salah.');
-            }
-        }
+    db.query('SELECT is_employers FROM users WHERE username = $1 AND password = $2', [username, password], pgp.queryResult.one)
+    .then(result => {
+        res.redirect('/home/?name=' + encodeURIComponent(req.body.username));
+    })
+    .catch(error => {
+        // Handle any errors
+        console.error(error);
+        res.send('Username atau password salah.');
     });
 });
 
