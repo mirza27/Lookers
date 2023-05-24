5491ca6ed68ceb4577018049d1da6de13d341ec8
andraalayubi <59492315+andraalayubi@users.noreply.github.com>
2023-05-24 22:08:14 +0700

    Update homeHandler.js

--- routes/homeHandler.js
+++ routes/homeHandler.js
@@ -6,10 +6,10 @@ app.set('view engine', 'ejs');
 
 
 const home = async (req, res) => {
-  if (req.method === 'GET' && !req.session.userId) {
-    return res.redirect('/login'); // jika belum login, redirect ke halaman login
+  //if (req.method === 'GET' && !req.session.userId) {
+    //return res.redirect('/login'); // jika belum login, redirect ke halaman login
 
-  } else{
+  //} else{
     // JIKA SEBAGAI JOB SEEKER
     if (req.method === 'GET' && !req.session.roleHRD){  // load home sebagai jobseeker
       console.log(req.session.userId); // cetak id siapa yang login.
@@ -17,7 +17,6 @@ const home = async (req, res) => {
         // Lakukan query ke database untuk mendapatkan data card
         const jobs = await db.any('SELECT * FROM jobs');
 
-        console.log(jobs); //tes
         // Render file EJS 'cards.ejs' dan kirimkan data dari query
         res.render('home.ejs', { jobs: jobs });
       } catch (err) {
@@ -26,29 +25,7 @@ const home = async (req, res) => {
       }
 
     } else if (req.method === 'POST' && !req.session.roleHRD){
-      try {
-    const searchTerm = req.query.keyword; // Kata yang diinputkan
-
-    const query = `
-      SELECT * FROM jobs
-      WHERE name LIKE '%${searchTerm}%'
-    `;
-
-    const results = await new Promise((resolve, reject) => {
-      connection.query(query, (err, results) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(results);
-        }
-      });
-    });
-
-    res.render('home', { jobs: results });
-  } catch (err) {
-    console.error('Error dalam melakukan query: ', err);
-    res.status(500).json({ error: 'Terjadi kesalahan saat melakukan query' });
-  }
+      
       }
     
     // JIKA SEBAGAI HRD
@@ -65,33 +42,45 @@ const home = async (req, res) => {
         res.status(500).json({ error: 'Terjadi kesalahan saat mengambil data jobs' });
       }
     }else if (req.method === 'POST' && req.session.roleHRD){
-      try {
-    const searchTerm = req.query.keyword; // Kata yang diinputkan
-
-    const query = `
-      SELECT * FROM jobs
-      WHERE name LIKE '%${searchTerm}%'
-    `;
-
-    const results = await new Promise((resolve, reject) => {
-      connection.query(query, (err, results) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(results);
-        }
-      });
-    });
-
-    res.render('home', { jobs: results });
-  } catch (err) {
-    console.error('Error dalam melakukan query: ', err);
-    res.status(500).json({ error: 'Terjadi kesalahan saat melakukan query' });
-  }
+      
     }
 
   }
+  //}
+
+const search  = async (req, res) => {
+  if (req.method==='GET'){
+    try {
+      // Lakukan query ke database untuk mendapatkan data jobs
+      const jobs = await db.any('SELECT * FROM jobs');
+
+      // Render file EJS 'home.ejs' dan kirimkan data dari query
+      res.render('search.ejs', { jobs: jobs });
+    } catch (err) {
+      console.error('Error dalam melakukan query: ', err);
+      res.status(500).json({ error: 'Terjadi kesalahan saat mengambil data jobs' });
+    }
+  }else if(req.method==='POST'){
+    try {
+      const searchTerm = req.body.search; // Kata yang diinputkan
+      console.log('seacrh'+search);  
+       
+        // Lakukan query ke database untuk mendapatkan data card
+        const query = await db.any(`
+          SELECT * FROM jobs
+          WHERE tittle ILIKE '%${search}%'
+        `);
+
+        console.log('hasil'+query);
+        // Render file EJS 'cards.ejs' dan kirimkan data dari query
+        res.render('search.ejs', { jobs: query });
+       
+    } catch (err) {
+      console.error('Error dalam melakukan query: ', err);
+      res.status(500).json({ error: 'Terjadi kesalahan saat melakukan query' });
+    }
   }
+}
 
 const inbox = async (req, res) => {
   try {
@@ -105,6 +94,8 @@ const inbox = async (req, res) => {
     res.status(500).json({ error: 'Terjadi kesalahan saat mengambil data pelamar' });
   }
 }
+
 module.exports = {
-  home
+  home,
+  search
 }
