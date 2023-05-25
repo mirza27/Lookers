7c6beac66d5d538e9397fea9669dbd1ded4a6097
andraalayubi <59492315+andraalayubi@users.noreply.github.com>
2023-05-25 20:29:45 +0700

    Create jobHandler.js

---
+++ routes/jobHandler.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const db = require('../db/db');
+app.set('view engine', 'ejs');
+
+const addJob = async (req, res) => {
+    if(req.method==='GET'){
+        res.render(`addJob.ejs`);
+    }else if (req.method === 'POST') {
+        const { tittle, category, desc, salary_min, salary_max, location, exp } = req.body;
+        try {
+            const is_done = false;
+            await db.query(`INSERT INTO jobs VALUES (${req.session.userId}, ${tittle}, ${category}, ${desc}, ${salary_min}, ${salary_max}, ${location}, ${exp}, ${is_done})`);
+
+            res.render(`addJob.ejs`);
+        } catch {
+            console.error('Error dalam fungsi addJob: ', err);
+            res.status(500).json({ error: 'Terjadi kesalahan saat menambah job' });
+        }
+    }
+}
+
+const addApp = async (req, res) => {
+    if(req.method==='GET'){
+        res.render(`addApp.ejs`);
+    } else if(req.method === 'POST') {
+        const status = false;
+        try {
+            await db.query(`INSERT INTO applications VALUES (${req.session.userId}, ${req.body.job_id}, ${status})`);
+            res.redirect(`/home`);
+            res.send(`
+                <script>alert('Application Berhasil!.');</script>
+            `);
+        } catch {
+            console.error('Error dalam fungsi addApp: ', err);
+            res.status(500).json({ error: 'Terjadi kesalahan saat melamar job' });
+        }
+    }
+}
+
+module.exports = {
+    addJob,
+    addApp
+}
