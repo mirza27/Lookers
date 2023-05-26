69025ff29765b465ef6f191c6afdee6389129c88
mirza27 <ramadhanimirza10@gmail.com>
2023-05-26 15:56:51 +0700

    nee login done

--- routes/inboxHandler.js
+++ routes/inboxHandler.js
@@ -4,80 +4,89 @@ const db = require('../db/db');
 app.set('view engine', 'ejs');
 
 const inbox = async (req, res) => {
-    if (req.method === 'GET' && req.session.roleHRD) {
-        try {
-            console.log("session id",req.session.userId)
-          
-            
-            // Menjalankan query untuk mendapatkan daftar pelamar
-
-            const results = await db.any( `SELECT jobseekers.jobseeker_id, jobseekers.name,  jobseeker_detail."exp", experience, education FROM jobseeker_detail
-            JOIN jobseekers ON jobseekers.jobseeker_id =  jobseeker_detail.jobseeker_id
-            JOIN applications ON jobseekers.jobseeker_id = applications.jobseeker_id 
-            JOIN jobs ON applications.job_id = jobs.job_id
-            JOIN employers ON jobs.employer_id = employers.employer_id 
-            WHERE employers.employer_id = ${req.session.userId} `);
-            //applicants.sessionUser = req.session.userName; // menambah data session ke ejs
-            
-            console.log(JSON.stringify(results))
-            res.render('inbox.ejs', { applicants: results });
-
-        } catch (err) {
-            console.error('Error dalam melakukan query: ', err);
-            res.status(500).json({ error: 'Terjadi kesalahan saat mengambil data pelamar' });
+    if (req.method === 'GET' && !req.session.userId) {
+        return res.redirect('/login'); // jika belum login, redirect ke halaman login
+      
+        } else{
+            if (req.method === 'GET' && req.session.roleHRD) {
+                try {
+                    
+                    // Menjalankan query untuk mendapatkan daftar pelamar
+        
+                    const results = await db.any( `SELECT jobs.tittle, jobseekers.jobseeker_id, jobseekers.name,  jobseeker_detail."exp", experience, education FROM jobseeker_detail
+                    JOIN jobseekers ON jobseekers.jobseeker_id =  jobseeker_detail.jobseeker_id
+                    JOIN applications ON jobseekers.jobseeker_id = applications.jobseeker_id 
+                    JOIN jobs ON applications.job_id = jobs.job_id
+                    JOIN employers ON jobs.employer_id = employers.employer_id 
+                    WHERE employers.employer_id = ${req.session.userId} `);
+                    //applicants.sessionUser = req.session.userName; // menambah data session ke ejs 
+                   
+                    res.render('inbox.ejs', { applicants: results });
+        
+                } catch (err) {
+                    console.error('Error dalam melakukan query: ', err);
+                    res.status(500).json({ error: 'Terjadi kesalahan saat mengambil data pelamar' });
+                }
+            } else if (req.method === 'POST' && req.session.roleHRD) {
+                const { sAcc, sRej } = req.body;
+        
+                if (sAcc && parseInt(sAcc)) {
+                    const userId = parseInt(sAcc);
+                    // Perform the database update code to set Accept
+                    db.query(`UPDATE applications SET status = "Accept" WHERE jobseeker_id = ${userId}`);
+                }
+                if (sRej && parseInt(sRej)) {
+                    const userId = parseInt(sRej);
+                    // Perform the database update code to set Reject
+                    db.query(`UPDATE applications SET status = "Reject" WHERE jobseeker_id = ${userId}`);
+                }
+                res.render('inbox.ejs');
+            } else if (req.method === 'GET' && !req.session.roleHRD) {
+                try {
+                    const id = req.session.userId;
+                    // Menjalankan query untuk mendapatkan daftar pelamar
+                    const sql = `SELECT jobs.job_id, 'jobs.tittle', status FROM applications
+                    JOIN jobs ON applications.application_id = jobs.job_id
+                    WHERE applications.jobseeker_id ='${id}'`;
+                    const { applicants } = await db.query(sql);
+        
+                    res.render('inbox.ejs', { applicants: applicants });
+                } catch (err) {
+                    console.error('Error dalam melakukan query: ', err);
+                    res.status(500).json({ error: 'Terjadi kesalahan saat mengambil data pelamar' });
+                }
+            }
         }
-    } else if (req.method === 'POST' && req.session.roleHRD) {
-        const { sAcc, sRej } = req.body;
-
-        if (sAcc && parseInt(sAcc)) {
-            const userId = parseInt(sAcc);
-            // Perform the database update code to set Accept
-            db.query(`UPDATE applications SET status = "Accept" WHERE jobseeker_id = ${userId}`);
-        }
-        if (sRej && parseInt(sRej)) {
-            const userId = parseInt(sRej);
-            // Perform the database update code to set Reject
-            db.query(`UPDATE applications SET status = "Reject" WHERE jobseeker_id = ${userId}`);
-        }
-        res.render('inbox.ejs');
-    } else if (req.method === 'GET' && !req.session.roleHRD) {
-        try {
-            const id = req.session.userId;
-            // Menjalankan query untuk mendapatkan daftar pelamar
-            const sql = `SELECT jobs.job_id, 'jobs.tittle', status FROM applications
-            JOIN jobs ON applications.application_id = jobs.job_id
-            WHERE applications.jobseeker_id ='${id}'`;
-            const { applicants } = await db.query(sql);
-
-            res.render('inbox.ejs', { applicants: applicants });
-        } catch (err) {
-            console.error('Error dalam melakukan query: ', err);
-            res.status(500).json({ error: 'Terjadi kesalahan saat mengambil data pelamar' });
-        }
-    }
+    
 }
 
 const myJobs = async (req, res) => {
-    if(req.method === 'GET'){
-        try{
-            const jobs = await db.any(`SELECT jobs.job_id, jobs.tittle, jobs.desc, jobs.salary_min, jobs.salary_max, jobs.location, jobs.exp, categories.name
-            FROM categories
-            JOIN jobs ON categories.category_id = jobs.category_id
-            WHERE jobs.employer_id = ${req.session.userId};`);
-            res.render(`myJobs.ejs`, { jobs: jobs });
-        }catch(err){
-            console.error('Error dalam melakukan query: ', err);
-            res.status(500).json({ error: 'Terjadi kesalahan saat mengambil data pekerjaan' });
+    if (req.method === 'GET' && !req.session.userId) {
+        return res.redirect('/login'); // jika belum login, redirect ke halaman login
+      
+        } else{
+            if(req.method === 'GET'){
+                try{
+                    const jobs = await db.any(`SELECT jobs.job_id, jobs.tittle, jobs.desc, jobs.salary_min, jobs.salary_max, jobs.location, jobs.exp, categories.name
+                    FROM categories
+                    JOIN jobs ON categories.category_id = jobs.category_id
+                    WHERE jobs.employer_id = ${req.session.userId};`);
+                    res.render(`myJobs.ejs`, { jobs: jobs });
+                }catch(err){
+                    console.error('Error dalam melakukan query: ', err);
+                    res.status(500).json({ error: 'Terjadi kesalahan saat mengambil data pekerjaan' });
+                }
+            }else if(req.method==='POST'){
+                try{
+                    await db.query(`UPDATE jobs SET is_done = true WHERE job_id = ${req.body.is_done}`);
+                    res.redirect(`/home/myJobs`);
+                }catch(err){
+                    console.error('Error dalam melakukan query: ', err);
+                    res.status(500).json({ error: 'Terjadi kesalahan saat mengubah data pekerjaan' });
+                }
+            }
         }
-    }else if(req.method==='POST'){
-        try{
-            await db.query(`UPDATE jobs SET is_done = true WHERE job_id = ${req.body.is_done}`);
-            res.redirect(`/home/myJobs`);
-        }catch(err){
-            console.error('Error dalam melakukan query: ', err);
-            res.status(500).json({ error: 'Terjadi kesalahan saat mengubah data pekerjaan' });
-        }
-    }
+   
 }
 
 module.exports = {
--- routes/jobHandler.js
+++ routes/jobHandler.js
@@ -4,58 +4,70 @@ const db = require('../db/db');
 app.set('view engine', 'ejs');
 
 const addJob = async (req, res) => {
-    if (req.method === 'GET') {
-        res.render(`addJob.ejs`);
-    } else if (req.method === 'POST') {
-        const { id, tittle, category, desc, salary_min, salary_max, location, exp } = req.body;
-        let category_id;
-        switch (category) {
-            case 'Teacher':
-                category_id=1;
-                break;
-            case 'Engineer':
-                category_id=2;
-                break;
-            case 'Chef':
-                category_id=3;
-                break;
-            case 'IT':
-                category_id=4;
-                break;
-            case 'Accountant':
-                category_id=5;
-                break;
-            default:
-                category_id=0;
-                break;
+    if (req.method === 'GET' && !req.session.userId) {
+        return res.redirect('/login'); // jika belum login, redirect ke halaman login
+      
+        } else{
+            if (req.method === 'GET') {
+                res.render(`addJob.ejs`);
+            } else if (req.method === 'POST') {
+                const { id, tittle, category, desc, salary_min, salary_max, location, exp } = req.body;
+                let category_id;
+                switch (category) {
+                    case 'Teacher':
+                        category_id=1;
+                        break;
+                    case 'Engineer':
+                        category_id=2;
+                        break;
+                    case 'Chef':
+                        category_id=3;
+                        break;
+                    case 'IT':
+                        category_id=4;
+                        break;
+                    case 'Accountant':
+                        category_id=5;
+                        break;
+                    default:
+                        category_id=0;
+                        break;
+                }
+        
+                try {
+                    const is_done = false;
+                    await db.query(`INSERT INTO jobs VALUES (${id}, ${category_id}, ${req.session.userId}, '${tittle}', '${desc}', ${salary_min}, ${salary_max}, '${location}', ${exp}, '${is_done}')`);
+                    //res.send(`<script>alert('Add Job Berhasil!.');</script>`);
+                    res.redirect(`/home/`);
+                } catch (err) {
+                    console.error('Error dalam fungsi addJob: ', err);
+                    res.status(500).json({ error: 'Terjadi kesalahan saat menambah job' });
+                }
+            }
         }
-
-        try {
-            const is_done = false;
-            await db.query(`INSERT INTO jobs VALUES (${id}, ${category_id}, ${req.session.userId}, '${tittle}', '${desc}', ${salary_min}, ${salary_max}, '${location}', ${exp}, '${is_done}')`);
-            //res.send(`<script>alert('Add Job Berhasil!.');</script>`);
-            res.redirect(`/home/`);
-        } catch (err) {
-            console.error('Error dalam fungsi addJob: ', err);
-            res.status(500).json({ error: 'Terjadi kesalahan saat menambah job' });
-        }
-    }
+    
 }
 
 const addApp = async (req, res) => {
-    if (req.method === 'GET') {
-        res.render(`addApp.ejs`);
-    } else if (req.method === 'POST') {
-        const status = false;
-        try {
-            await db.query(`INSERT INTO applications VALUES (${req.session.userId}, ${req.body.job_id}, ${status})`);
-            res.redirect(`/home`);
-            //res.send(`<script>alert('Add Aplicant Berhasil!.');</script>`);
-        } catch (err) {
-            console.error('Error dalam fungsi addApp: ', err);
-            res.status(500).json({ error: 'Terjadi kesalahan saat melamar job' });
+    if (req.method === 'GET' && !req.session.userId) {
+        return res.redirect('/login'); // jika belum login, redirect ke halaman login
+      
+        } else{
+            if (req.method === 'GET') {
+                res.render(`addApp.ejs`);
+            } else if (req.method === 'POST') {
+                const status = false;
+                try {
+                    await db.query(`INSERT INTO applications VALUES (${req.session.userId}, ${req.body.job_id}, ${status})`);
+                    res.redirect(`/home`);
+                    //res.send(`<script>alert('Add Aplicant Berhasil!.');</script>`);
+                } catch (err) {
+                    console.error('Error dalam fungsi addApp: ', err);
+                    res.status(500).json({ error: 'Terjadi kesalahan saat melamar job' });
+                }
+            }
         }
-    }
+    
 }
 
 module.exports = {
