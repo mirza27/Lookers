30a9ffe69d6f74472ad3a2ec9b62922c2723394d
mirza27 <ramadhanimirza10@gmail.com>
2023-05-29 00:15:31 +0700

    myApply works

--- app.js
+++ app.js
@@ -28,38 +28,40 @@ app.use(cookieParser());
 
 
 // PATH ENDPOINT
-app.get("/login", userHandler.userLogin); 
+app.get("/login", userHandler.userLogin); // Login
 app.post("/login", userHandler.userLogin);
 
-app.get("/logout", userHandler.userLogout);
+app.get("/logout", userHandler.userLogout); // logout
 
-app.get("/register", userHandler.userRegister);
+app.get("/register", userHandler.userRegister); // register awal
 app.post("/register", userHandler.userRegister);
 
-app.get("/register/hrd/:id", userHandler.regisHRD);
-app.post("/register/hrd/:id", userHandler.regisHRD);
+app.get("/register/hrd/:id", userHandler.regisHRD); // register sebagai hrd
+app.post("/register/hrd/:id", userHandler.regisHRD); 
 
-app.get("/register/js/:id", userHandler.regisJS);
+app.get("/register/js/:id", userHandler.regisJS); // register jobseeker
 app.post("/register/js/:id", userHandler.regisJS);
 
-app.get("/home", homeHandler.home);
+app.get("/home", homeHandler.home); // load home / dashboard
 app.post("/home", homeHandler.home);
 
-app.post("/home/search", homeHandler.home); 
+app.post("/home/search", homeHandler.home); // menampilkan jobs tersedia
 
-app.get("/profil", homeHandler.profil);
+app.get("/profil", homeHandler.profil); // edit profile js /hrd
 app.post("/profil", homeHandler.profil);
 
-app.get("/home/inbox", inboxHandler.inbox);
+app.get("/home/inbox", inboxHandler.inbox); // inbox hrd / lamaran yang diterima hrd
 app.post("/home/inbox", inboxHandler.inbox);
 
-app.get("/home/myJobs", inboxHandler.myJobs);
+app.get("/home/myApply", inboxHandler.myApply); // daftar lamaran (js)
+
+app.get("/home/myJobs", inboxHandler.myJobs); // loker yang dibuat hrd
 app.post("/home/myJobs", inboxHandler.myJobs);
 
-app.get("/home/addJobs", jobHandler.addJob);
+app.get("/home/addJobs", jobHandler.addJob); // menambah loker
 app.post("/home/addJobs", jobHandler.addJob);
 
-app.get("/home/addApp", jobHandler.addApp);
+app.get("/home/addApp", jobHandler.addApp); // membuat lamaran (js)
 app.post("/home/addApp", jobHandler.addApp);
 
 // Jalankan server
--- routes/inboxHandler.js
+++ routes/inboxHandler.js
@@ -3,6 +3,7 @@ const app = express();
 const db = require('../db/db');
 app.set('view engine', 'ejs');
 
+// Halaman inbox saya (hrd)
 const inbox = async (req, res) => {
     if (req.method === 'GET' && !req.session.userId) {
         return res.redirect('/login'); // jika belum login, redirect ke halaman login
@@ -12,13 +13,12 @@ const inbox = async (req, res) => {
                 try {
                     
                     // Menjalankan query untuk mendapatkan daftar pelamar
-        
                     const results = await db.any( `SELECT jobs.tittle, jobseekers.jobseeker_id, jobseekers.name,  jobseeker_detail."exp", experience, education FROM jobseeker_detail
                     JOIN jobseekers ON jobseekers.jobseeker_id =  jobseeker_detail.jobseeker_id
                     JOIN applications ON jobseekers.jobseeker_id = applications.jobseeker_id 
                     JOIN jobs ON applications.job_id = jobs.job_id
                     JOIN employers ON jobs.employer_id = employers.employer_id 
-                    WHERE employers.employer_id = ${req.session.userId} `);
+                    WHERE employers.employer_id = ${req.session.userId} AND applications.status = 'waiting';`);
                     //applicants.sessionUser = req.session.userName; // menambah data session ke ejs 
                    
                     res.render('inbox.ejs', { applicants: results });
@@ -29,35 +29,48 @@ const inbox = async (req, res) => {
                 }
             } else if (req.method === 'POST' && req.session.roleHRD) {
                 const { sAcc, sRej } = req.body;
-        
+                
+
+                // menerima / menolak lamaran
                 if (sAcc && parseInt(sAcc)) {
                     const userId = parseInt(sAcc);
                     // Perform the database update code to set Accept
-                    db.query(`UPDATE applications SET status = "Accept" WHERE jobseeker_id = ${userId}`);
+                    db.query(`UPDATE applications SET status = "accepted" WHERE jobseeker_id = ${userId}`);
+
+                    res.redirect("/home/inbox") // load lagi halaman
                 }
                 if (sRej && parseInt(sRej)) {
                     const userId = parseInt(sRej);
                     // Perform the database update code to set Reject
-                    db.query(`UPDATE applications SET status = "Reject" WHERE jobseeker_id = ${userId}`);
+                    db.query(`UPDATE applications SET status = "rejected" WHERE jobseeker_id = ${userId}`);
+
+                    res.redirect("/home/inbox")  // load lagi halaman
                 }
-                res.render('inbox.ejs');
-            } else if (req.method === 'GET' && !req.session.roleHRD) {
-                try {
-                    const id = req.session.userId;
-                    // Menjalankan query untuk mendapatkan daftar pelamar
-                    const sql = `SELECT jobs.job_id, 'jobs.tittle', status FROM applications
-                    JOIN jobs ON applications.application_id = jobs.job_id
-                    WHERE applications.jobseeker_id ='${id}'`;
-                    const { applicants } = await db.query(sql);
-        
-                    res.render('inbox.ejs', { applicants: applicants });
-                } catch (err) {
-                    console.error('Error dalam melakukan query: ', err);
-                    res.status(500).json({ error: 'Terjadi kesalahan saat mengambil data pelamar' });
-                }
+        }
+}
+}
+
+// Halaman lamaran saya
+const myApply = async (req, res) => {
+    if (req.method === 'GET' && !req.session.userId) {
+        return res.redirect('/login'); // jika belum login, redirect ke halaman login
+      
+        } else{
+            try {
+                const id = req.session.userId;
+                // Menjalankan query untuk mendapatkan daftar pelamar
+                const sql = `SELECT application_id, jobs.tittle, status, employers.company_name FROM applications
+                JOIN jobs ON applications.job_id = jobs.job_id
+                JOIN employers ON employers.employer_id = jobs.employer_id
+                           WHERE applications.jobseeker_id = ${id}`;
+                const applicants = await db.query(sql);
+    
+                res.render('myApply.ejs', { applicants: applicants });
+            } catch (err) {
+                console.error('Error dalam melakukan query: ', err);
+                res.status(500).json({ error: 'Terjadi kesalahan saat mengambil data pelamar' });
             }
         }
-    
 }
 
 const myJobs = async (req, res) => {
@@ -91,5 +104,6 @@ const myJobs = async (req, res) => {
 
 module.exports = {
     inbox,
-    myJobs
+    myJobs,
+    myApply
 }
